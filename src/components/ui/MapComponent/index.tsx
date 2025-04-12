78cb189ff2eddecabc7d7fb7d4b92db419176db1
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2025-04-12 13:36:28 +0500

    updated map marker

--- src/components/ui/MapComponent/index.tsx
+++ src/components/ui/MapComponent/index.tsx
@@ -16,13 +16,12 @@ interface MapComponentProps {
 const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
 
 export default function MapComponent({ address }: MapComponentProps) {
-  const [marker, setMarker] = useState<[number, number] | null>(null);
+  const [marker, setMarker] = useState<[number, number]>([46.6753, 24.7136]);
   const mapRef = useRef<MapRef | null>(null);
 
   useEffect(() => {
     const fetchCoordinates = async () => {
-      if (!address || address.length < 5) return;
-
+      if (!address || address.length < 4) return;
       try {
         const res = await fetch(
           `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
@@ -31,11 +30,14 @@ export default function MapComponent({ address }: MapComponentProps) {
         );
 
         const data = await res.json();
+        console.log("data: ", data);
         const coordinates = data?.features?.[0]?.center;
 
         if (coordinates) {
           setMarker(coordinates);
-          mapRef.current?.flyTo({ center: coordinates, zoom: 10 });
+          mapRef.current?.flyTo({ center: coordinates, zoom: 7 });
+        } else {
+          console.warn("No coordinates found for address:", address);
         }
       } catch (error) {
         console.error("Geocoding failed:", error);
