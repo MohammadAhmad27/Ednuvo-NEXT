add201d362998eee19c0cf27063ff82026bd0017
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2025-04-13 19:21:34 +0500

    added <ClientComponent />

--- src/app/service-requester-dashboard/page.tsx
+++ src/app/service-requester-dashboard/page.tsx
@@ -1,49 +1,10 @@
-"use client";
-import { useEffect, Suspense } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import RequesterDashboard from "@/components/ServiceRequester-Dashboard/RequesterDashboard";
-import RequesterJobs from "@/components/ServiceRequester-Dashboard/RequesterJobs";
-import RequesterDisputes from "@/components/ServiceRequester-Dashboard/RequesterDisputes";
-import RequesterMessages from "@/components/ServiceRequester-Dashboard/RequesterMessages";
-
-const RenderTabComponent = ({ tab }: { tab: string | null }) => {
-  switch (tab) {
-    case "dashboard":
-      return <RequesterDashboard />;
-    case "jobs":
-      return <RequesterJobs />;
-    case "disputes":
-      return <RequesterDisputes />;
-    case "messages":
-      return <RequesterMessages />;
-    default:
-      return null;
-  }
-};
-
-const PageContent = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const tab = searchParams.get("view");
-  const validTabs = ["dashboard", "jobs", "disputes", "messages"];
-
-  useEffect(() => {
-    if (!tab || !validTabs.includes(tab)) {
-      router.replace("/service-requester-dashboard?view=dashboard");
-    }
-  }, [tab, router]);
-
-  return (
-    <div className="w-full overflow-hidden bg-lightgreen rounded-tl-[40px] pl-5 py-5 pr-12">
-      <RenderTabComponent tab={tab} />
-    </div>
-  );
-};
+import { Suspense } from "react";
+import ClientComponent from "./ClientComponent";
 
 const page = () => {
   return (
     <Suspense fallback={<div className="pl-5 py-5 pr-12">Loading...</div>}>
-      <PageContent />
+      <ClientComponent />
     </Suspense>
   );
 };
