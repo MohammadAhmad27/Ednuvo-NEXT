cb0beb1fb308282182e3906cdb73fa672b9102a5
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2025-04-13 18:31:44 +0500

    fixed build issue

--- src/app/service-requester-dashboard/page.tsx
+++ src/app/service-requester-dashboard/page.tsx
@@ -1,17 +1,32 @@
 "use client";
-import { useEffect } from "react";
+
+import { useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import RequesterDashboard from "@/components/ServiceRequester-Dashboard/RequesterDashboard";
 import RequesterJobs from "@/components/ServiceRequester-Dashboard/RequesterJobs";
 import RequesterDisputes from "@/components/ServiceRequester-Dashboard/RequesterDisputes";
 import RequesterMessages from "@/components/ServiceRequester-Dashboard/RequesterMessages";
 
-const validTabs = ["dashboard", "jobs", "disputes", "messages"];
+const RenderTabComponent = ({ tab }: { tab: string | null }) => {
+  switch (tab) {
+    case "dashboard":
+      return <RequesterDashboard />;
+    case "jobs":
+      return <RequesterJobs />;
+    case "disputes":
+      return <RequesterDisputes />;
+    case "messages":
+      return <RequesterMessages />;
+    default:
+      return null;
+  }
+};
 
-const Page = () => {
+const PageContent = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const tab = searchParams.get("view");
+  const validTabs = ["dashboard", "jobs", "disputes", "messages"];
 
   useEffect(() => {
     if (!tab || !validTabs.includes(tab)) {
@@ -19,26 +34,19 @@ const Page = () => {
     }
   }, [tab, router]);
 
-  const RenderTabComponent = ({ tab }: { tab: string | null }) => {
-    switch (tab) {
-      case "dashboard":
-        return <RequesterDashboard />;
-      case "jobs":
-        return <RequesterJobs />;
-      case "disputes":
-        return <RequesterDisputes />;
-      case "messages":
-        return <RequesterMessages />;
-      default:
-        return null;
-    }
-  };
-
   return (
     <div className="w-full overflow-hidden bg-lightgreen rounded-tl-[40px] pl-5 py-5 pr-12">
-      {<RenderTabComponent tab={tab} />}
+      <RenderTabComponent tab={tab} />
     </div>
   );
 };
 
-export default Page;
+const page = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PageContent />
+    </Suspense>
+  );
+};
+
+export default page;
